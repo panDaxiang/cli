2d998333ccd677562710e105abd809d1864ac03e
panxiang <panxiang@finchina.com>
2022-05-24 20:03:12 +0800

    脚手架基础配置

---
+++ bin/cli.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const { program } = require('commander');
+const inquirer = require('inquirer');
+const create = require('../lib/create')
+
+// program.version(require('../package.json').version).parse(process.argv)
+
+program.option('-d, --debug', 'debug comd')
+
+const question = [
+  {
+    name: 'name',
+    message: '请输入项目名称？',
+  }, {
+    name: 'author',
+    message: '请输入作者？',
+  }
+]
+
+
+program.command('create').description('create a project').action((data) => {
+  inquirer.prompt(question).then((res) => {
+    create(res)
+  })
+})
+
+program.parse(process.argv)
+
+const options = program.opts();
+
+if (options.debug) {
+  console.log('enter debug comd')
+}
